6278243919491ce8e411c99abf0ee13e5829a300
scibuff <contact@tomasvorobjov.com>
2020-01-21 00:46:53 +0100

    - version 1.1.0.1 - added guild treasury - added leaderboard results after league is over

--- panel.js
+++ panel.js
@@ -195,6 +195,10 @@ var foe = jQuery.createNamespace( 'com.scibuff.foe.gbg' );
                     left: 'players-1-40',
                     right: 'players-41-80'
                 },
+                treasury : {
+                    left: 'treasury-left',
+                    right: 'treasury-right'
+                },
                 update: 'for-gbg-last-update'
             }
         };
@@ -221,12 +225,39 @@ var foe = jQuery.createNamespace( 'com.scibuff.foe.gbg' );
                 if ( o && o.requestClass && o.requestClass == "GuildBattlegroundService" ){
 
                     if ( o.requestMethod == "getPlayerLeaderboard" ){
-                        // specific building
                         tmp.processLeaderboard( o.responseData );
+                    }
+                }
+                if ( o && o.requestClass && o.requestClass == "ClanService" ){
+
+                    if ( o.requestMethod == "getTreasury" ){
+                        tmp.processTreasury( o.responseData );
+                    }
+                }
+                if ( o && o.requestClass && o.requestClass == "GuildBattlegroundStateService" ) {
+
+                    if ( o.requestMethod == "getState" ){
+
+                        if ( o && o.responseData && o.responseData.playerLeaderboardEntries ) {
+                            tmp.processLeaderboard( o.responseData.playerLeaderboardEntries );
+                        }
+                        else {
+                            debug('no o.responseData.playerLeaderboardEntries data');
+                        }
                     }
                 }
             }
         };
+
+        tmp.processTreasury = function ( data ){
+
+            if ( !data || !data.resources ){
+                console.log('no resource data');
+                return;
+            }
+            foe.treasury.setData( data.resources );
+            tmp.printTreasury();
+        }
 
         /**
          *
@@ -328,6 +359,63 @@ var foe = jQuery.createNamespace( 'com.scibuff.foe.gbg' );
 
         };
 
+        tmp.printTreasury = function(){
+
+            var bodyLeft = $('#' + tmp.options.html.ids.treasury.left + ' > tbody' );
+            var bodyRight = $('#' + tmp.options.html.ids.treasury.right + ' > tbody' );
+
+            bodyLeft.empty();
+            bodyRight.empty();
+
+            var treasury = foe.treasury.getData();
+
+            var lefts = [];
+            var rights = [];
+
+            var size = Object.keys(treasury).length;
+
+            for (var i = 1; i <= size; i++ ) {
+                var era = treasury[i];
+                if ( era ) {
+                    for ( var j = 1; j <= 5; j++ ) {
+                        if ( i % 2 == 1 ) {
+                            lefts.push( era[j] );
+                        }
+                        else {
+                            rights.push( era[j] );
+                        }
+                    }
+                }
+            }
+
+            // print left
+            for ( var i = 0; i < lefts.length; i++ ){
+                tmp.aux.printTreasuryRow( bodyLeft, lefts[i] );
+
+            }
+            // print right
+            for ( var i = 0; i < rights.length; i++ ){
+                tmp.aux.printTreasuryRow( bodyRight, rights[i] );
+
+            }
+        };
+
+        tmp.aux.printTreasuryRow = function(parent,item){
+
+            var html = '<tr>'
+                + '<td class="resource-icon">&nbsp;</td>'
+                + '<td class="resource-name">' + item.name + '</td>'
+                + '<td class="resource-value">' + tmp.aux.formatTresuryValue( item.value ) + '</td>'
+                + '</tr>';
+            var tr = $( html );
+
+            parent.append(tr);
+
+        };
+        tmp.aux.formatTresuryValue = function(n){
+            return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        }
+
         tmp.buttons = { delete: null, take: null, load: null };
         tmp.takeSnapshot = function(){
             foe.data.takeSnapshot();
@@ -379,12 +467,163 @@ var foe = jQuery.createNamespace( 'com.scibuff.foe.gbg' );
         return pub;
     }();
 
+    foe.treasury = function(){
+        var tmp = {};
+        tmp.treasury = {};
+
+        var pub = {};
+        pub.setData = function( resources ){
+
+            var raw = {};
+
+            for ( var resource in resources ){
+                var o = tmp.goods[resource];
+                if ( o && o.era ){
+                    if ( !raw[ o.era ] ){ raw[ o.era ] = {}; }
+                    o.value = resources[ resource ];
+                    raw[ o.era ][ o.order ] = o;
+                }
+            }
+            tmp.treasury = raw;
+
+            if ( ! tmp.treasury['1'] ){
+                tmp.treasury['1'] = {
+                    '1': {era:1,order:1,name:'Marble',value:0},
+                    '2': {era:1,order:2,name:'Lumber',value:0},
+                    '3': {era:1,order:3,name:'Dye',value:0},
+                    '4': {era:1,order:4,name:'Stone',value:0},
+                    '5': {era:1,order:5,name:'Wine',value:0}
+                }
+            }
+
+        }
+        pub.getData = function(){
+            return tmp.treasury;
+        }
+
+        tmp.goods = {
+            guild_expedition_point: 0,
+            medals: 0,
+            guild_championship_trophy_gold: 0,
+
+            bronze: { era: 3, order: 1, name: 'Cooper'},
+            biotech_crops: { era: 17, order: 1, name: 'BioTech Crops'},
+            asbestos: { era: 8, order: 1, name: 'Asbestos'},
+            fusion_reactors: { era: 17, order: 2, name: 'Fusion Reactors'},
+            papercrete: { era: 12, order: 2, name: 'Papercrete'},
+            electromagnets: { era: 11, order: 2, name: 'Electromagnets'},
+            superconductors: { era: 13, order: 5, name: 'Superconductors'},
+            purified_water: { era: 13, order: 4, name: 'Purified Water'},
+            lubricants: { era: 17, order: 3, name: 'Lubricants'},
+            pearls: { era: 15, order: 4, name: 'Pearls'},
+            robots: { era: 11, order: 5, name: 'Robots'},
+            translucent_concrete: { era: 12, order: 5, name: 'Translucent Concrete'},
+            cloth: { era: 2, order: 1, name: 'Cloth'},
+            gas: { era: 11, order: 3, name: 'Gas'},
+            machineparts: { era: 8, order: 3, name: 'Machine Parts'},
+            bionics: { era: 11, order: 1, name: 'Bionics Data'},
+            brick: { era: 4, order: 1, name: 'Bricks'},
+            ropes: { era: 4, order: 4, name: 'Ropes'},
+            salt: { era: 4, order: 5, name: 'Salt'},
+            glass: { era: 4, order: 2, name: 'Glass'},
+            herbs: { era: 4, order: 3, name: 'Dried Herbs'},
+            tinplate: { era: 8, order: 5, name: 'Tinplate'},
+            superalloys: { era: 17, order: 5, name: 'Superalloys'},
+            tar: { era: 6, order: 4, name: 'Tar'},
+            algae: { era: 13, order: 1, name: 'Algae'},
+            biogeochemical_data: { era: 13, order: 2, name: 'Biogeochemical Data'},
+            wire: { era: 6, order: 5, name: 'Wires'},
+            fertilizer: { era: 7, order: 2, name: 'Fertilizer'},
+            bioplastics: { era: 14, order: 2, name: 'Bioplastics'},
+            smart_materials: { era: 12, order: 4, name: 'Smart Materials'},
+            nutrition_research: { era: 12, order: 1, name: 'Nutrition Research'},
+            nanoparticles: { era: 13, order: 3, name: 'Nanoparticles'},
+            data_crystals: { era: 16, order: 2, name: 'Data Crystals'},
+            gems: { era: 2, order: 3, name: 'Jewelry'},
+            lead: { era: 2, order: 4, name: 'Iron'},
+            ebony: { era: 2, order: 2, name: 'Ebony'},
+            plastics: { era: 11, order: 4, name: 'Plastics'},
+            explosives: { era: 8, order: 2, name: 'Explosives'},
+            honey: { era: 3, order: 4, name: 'Honey'},
+            biolight: { era: 15, order: 2, name: 'Biolight'},
+            plankton: { era: 15, order: 5, name: 'Plankton'},
+            artificial_scales: { era: 15, order: 1, name: 'Artificial Scales'},
+            corals: { era: 15, order: 3, name: 'Corals'},
+            ai_data: { era: 14, order: 1, name: 'AI data'},
+            mars_microbes: { era: 17, order: 4, name: 'Mars Microbes'},
+            paper: { era: 6, order: 2, name: 'Paper'},
+            nanowire: { era: 14, order: 3, name: 'Nanowires'},
+            paper_batteries: { era: 14, order: 4, name: 'Paper batteries'},
+            transester_gas: { era: 14, order: 5, name: 'Transester gas'},
+            gold: { era: 3, order: 2, name: 'Gold'},
+            coke: { era: 7, order: 1, name: 'Coke'},
+            coffee: { era: 6, order: 1, name: 'Coffee'},
+            golden_rice: { era: 16, order: 3, name: 'Golden Rice'},
+            rubber: { era: 7, order: 3, name: 'Rubber'},
+            cryptocash: { era: 16, order: 1, name: 'Cryptocash'},
+            tea_silk: { era: 16, order: 5, name: 'Tea Silk'},
+            talc: { era: 5, order: 5, name: 'Talc Powder'},
+            basalt: { era: 5, order: 1, name: 'Basalt'},
+            silk: { era: 5, order: 4, name: 'Silk'},
+            porcelain: { era: 6, order: 3, name: 'Porcelain'},
+            steel: { era: 10, order: 5, name: 'Steel'},
+            luxury_materials: { era: 9, order: 4, name: 'Luxury Materials'},
+            preservatives: { era: 12, order: 3, name: 'Preservatives'},
+            dna_data: { era: 10, order: 1, name: 'Genome Data'},
+            packaging: { era: 9, order: 5, name: 'Packaging'},
+            convenience_food: { era: 9, order: 1, name: 'Conveniece Food'},
+            filters: { era: 10, order: 2, name: 'Industrial Filters'},
+            renewable_resources: { era: 10, order: 3, name: 'Renewable Resources'},
+            petroleum: { era: 8, order: 4, name: 'Gasoline'},
+            ferroconcrete: { era: 9, order: 2, name: 'Ferroconcrete'},
+            semiconductors: { era: 10, order: 4, name: 'Semiconductors'},
+            granite: { era: 3, order: 3, name: 'Granite'},
+            flavorants: { era: 9, order: 3, name: 'Flavorants'},
+            whaleoil: { era: 7, order: 5, name: 'Whale Oil'},
+            nanites: { era: 16, order: 4, name: 'Nanites'},
+            textiles: { era: 7, order: 4, name: 'Textiles'},
+            brass: { era: 5, order: 2, name: 'Brass'},
+            marble: { era: 3, order: 5, name: 'Alabaster'},
+            gunpowder: { era: 5, order: 3, name: 'Gunpowder'},
+            limestone: { era: 2, order: 5, name: 'Limestone'},
+        }
+
+        return pub;
+    }();
+
     foe.utils = function(){
 
         // public fields
         //var pub = {};
         //return pub;
         return {};
+    }();
+
+    foe.ui = function(){
+        var tmp = {};
+        var pub = {};
+
+        pub.initialize = function(){
+
+            $('#nav td a:not(:first)').addClass('inactive');
+            $('#data .data-container').hide();
+            $('#data .data-container:first').show();
+
+            $('#nav td a').click(function(e){
+                e.preventDefault();
+                var data = $(this).attr('data');
+
+                if ( $(this).hasClass('inactive') ){
+                    $('#nav td a').addClass('inactive');
+                    $(this).removeClass('inactive');
+
+                    $('#data .data-container').hide();
+                    $('#' + data).fadeIn('slow');
+                }
+            });
+        }
+
+        return pub;
     }();
 
     foe.players = function(){
@@ -471,22 +710,8 @@ var manifest = chrome.runtime.getManifest();
 document.getElementById('foe-gbg-version').innerHTML = 'v' + manifest.version;
 
 $(document).ready(function() {
+    foe.ui.initialize();
     foe.data.initialize(function(){
         foe.net.initialize();
     });
 });
-
-/*
- document.querySelector('#button-test').addEventListener('click', function() {
- //debug('test button click');
- }, false);
-
-
- document.querySelector('#button-reload').addEventListener('click', function() {
- chrome.devtools.inspectedWindow.reload();
- }, false);
- */
-
-//debug('panel.js - loaded');
-//console.log('console: panel.js - loaded');
-//debug('panel.js - jquery: ' + jQuery );
